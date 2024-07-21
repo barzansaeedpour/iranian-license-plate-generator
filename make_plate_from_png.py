55bd1d4324b5c0f2104fa53d1c2bf54970855be5
barzansaeedpour <barzansaeedpour@gmail.com>
2024-07-21 13:15:39 +0330

    new method is working!

--- make_plate_from_png.py
+++ make_plate_from_png.py
@@ -21,38 +21,117 @@ numbers = [
            "position":[100, 30],
            "size":[60, 115],
            },
+          {"ch":'4',
+           "position":[100, 30],
+           "size":[60, 115],
+           },
+          {"ch":'5',
+           "position":[100, 30],
+           "size":[60, 115],
+           },
+          {"ch":'6',
+           "position":[100, 30],
+           "size":[60, 115],
+           },
+          {"ch":'7',
+           "position":[100, 30],
+           "size":[60, 115],
+           },
+          {"ch":'8',
+           "position":[100, 30],
+           "size":[60, 115],
+           },
+          {"ch":'9',
+           "position":[100, 30],
+           "size":[60, 115],
+           },
         ]
-mini_numbers = []
+mini_numbers = [
+          {"ch":'0',
+           "position":[100, 90],
+           "size":(40, 40),
+           },
+          {"ch":'1',
+           "position":[100, 50],
+           "size":(40, 100),
+           },
+          {"ch":'2',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'3',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'4',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'5',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'6',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'7',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'8',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'9',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+        ]
+
 chars = [ 
-         {"ch":'T',
-           "position":(240, 30),
-           "size":(120, 110),
+        #  {"ch":'T',
+        #    "position":(252, 30),
+        #    "size":(120, 110),
+        #    },
+        #  {"ch":'H',
+        #    "position":(275, 50),
+        #    "size":(80, 80),
+        #    },
+        #  {"ch":'D',
+        #    "position":(275, 50),
+        #    "size":(80, 80),
+        #    },
+        #  {"ch":'Q',
+        #    "position":(265, 40),
+        #    "size":(100, 100),
+        #    },
+         {"ch":'HE',
+           "position":(265, 40),
+           "size":(100, 100),
            },
-         {"ch":'H',
-           "position":(260, 50),
-           "size":(80, 80),
-           },
-        #  'D', 'Q', 'HE', 'J', 'L', 'M', 'SAD', 'SIN', 'TA', 'V', 'Y'
+        #  'HE', 'J', 'L', 'M', 'SAD', 'SIN', 'TA', 'V', 'Y'
          ]
 
 # Make a random choice from the list
 random_1 = random.choice(numbers)
 random_2 = random.choice(numbers)
+
 random_char = random.choice(chars)
 
-print("Random choice:", random_char)
+random_3 = random.choice(numbers)
+random_4 = random.choice(numbers)
+random_5 = random.choice(numbers)
+random_6 = random.choice(numbers)
+
+mini_random_1 = random.choice(mini_numbers)
+while mini_random_1["ch"]=='0':
+    mini_random_1 = random.choice(mini_numbers)
+mini_random_2 = random.choice(mini_numbers)
 
 
-# Open the image to be overlaid
-overlay0 = remove_background(Image.open("./chars/0.png"))
-overlay2 = remove_background(Image.open("./chars/2.png"))
-overlay5 = remove_background(Image.open("./chars/5.png"))
-overlay6 = remove_background(Image.open("./chars/6.png"))
-overlay7 = remove_background(Image.open("./chars/7.png"))
-overlay9 = remove_background(Image.open("./chars/9.png"))
-# overlaysin = remove_background(Image.open("./chars/SIN.png"))
+
 overlaych = remove_background(Image.open(f"./chars/{random_char['ch']}.png"))
-# Set the position where the overlay image will be placed
 
 overlay1 = remove_background(Image.open(f"./chars/{random_1['ch']}.png"))
 overlay1 = overlay1.resize(random_1['size']) 
@@ -60,7 +139,7 @@ background.paste(overlay1, [100, random_1['position'][1]])
 
 overlay2 = remove_background(Image.open(f"./chars/{random_2['ch']}.png"))
 overlay2 = overlay2.resize(random_2['size']) 
-background.paste(overlay2, [170, random_2['position'][1]])
+background.paste(overlay2, [180, random_2['position'][1]])
 
 # position = (160, 30)  # Specify the coordinates (x, y)
 # overlay5 = overlay5.resize((60, 115)) 
@@ -70,27 +149,40 @@ position = random_char['position']  # Specify the coordinates (x, y)
 overlaych = overlaych.resize(random_char['size']) 
 background.paste(overlaych, position)
 
-position = (380, 30)  # Specify the coordinates (x, y)
-overlay5 = overlay5.resize((60, 115)) 
-background.paste(overlay5, position)
+overlay3 = remove_background(Image.open(f"./chars/{random_3['ch']}.png"))
+overlay3 = overlay3.resize(random_3['size']) 
+background.paste(overlay3, [390, random_3['position'][1]])
 
-position = (460, 30)  # Specify the coordinates (x, y)
-overlay7 = overlay7.resize((60, 115)) 
-background.paste(overlay5, position)
+overlay4 = remove_background(Image.open(f"./chars/{random_4['ch']}.png"))
+overlay4 = overlay4.resize(random_4['size']) 
+background.paste(overlay4, [470, random_4['position'][1]])
 
-position = (550, 30)  # Specify the coordinates (x, y)
-overlay1 = overlay1.resize((40, 115)) 
-background.paste(overlay5, position)
+overlay5 = remove_background(Image.open(f"./chars/{random_5['ch']}.png"))
+overlay5 = overlay5.resize(random_5['size']) 
+background.paste(overlay5, [550, random_5['position'][1]])
 
-position = (662, 50)  # 4
-overlay2 = overlay2.resize((63, 100)) # 4
-# overlay1 = overlay1.resize((57, 100)) # 4
-background.paste(overlay5, position)
+overlay6 = remove_background(Image.open(f"./chars/{mini_random_1['ch']}.png"))
+overlay6 = overlay6.resize(mini_random_1['size']) 
+background.paste(overlay6, [655, mini_random_1['position'][1]])
+
+overlay7 = remove_background(Image.open(f"./chars/{mini_random_2['ch']}.png"))
+overlay7 = overlay7.resize(mini_random_2['size']) 
+background.paste(overlay7, [720, mini_random_2['position'][1]])
 
 
-position = (730, 90)  # Specify the coordinates (x, y)
-overlay0 = overlay0.resize((40, 40)) 
-background.paste(overlay5, position)
+# position = (550, 30)  # Specify the coordinates (x, y)
+# overlay1 = overlay1.resize((40, 115)) 
+# background.paste(overlay5, position)
+
+# position = (662, 50)  # 4
+# overlay2 = overlay2.resize() # 4
+# # overlay1 = overlay1.resize((57, 100)) # 4
+# background.paste(overlay5, position)
+
+
+# position = (730, 90)  # Specify the coordinates (x, y)
+# overlay0 = overlay0.resize((40, 40)) 
+# background.paste(overlay5, position)
 
 
 # Save the final image with the overlay
