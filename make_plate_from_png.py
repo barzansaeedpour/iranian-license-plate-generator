63a6f860ad025d75ffb32732c0b2681ed73627e6
barzansaeedpour <barzansaeedpour@gmail.com>
2024-07-22 09:26:27 +0330

    ein char is added

--- make_plate_from_png.py
+++ make_plate_from_png.py
@@ -1,9 +1,6 @@
 from PIL import Image
 from remove_background import remove_background
-
-# Open the background image
-background = Image.open("templates/savari2.png")
-
+# from remove_background import remove_background_HE
 
 import random
 
@@ -47,49 +44,57 @@ numbers = [
         #    },
         ]
 mini_numbers = [
-        #   {"ch":'0',
-        #    "position":[100, 90],
-        #    "size":(40, 40),
-        #    },
-        #   {"ch":'1',
-        #    "position":[100, 50],
-        #    "size":(40, 100),
-        #    },
-        #   {"ch":'2',
-        #    "position":[100, 50],
-        #    "size":(57, 100),
-        #    },
-        #   {"ch":'3',
-        #    "position":[100, 50],
-        #    "size":(57, 100),
-        #    },
-        #   {"ch":'4',
-        #    "position":[100, 50],
-        #    "size":(57, 100),
-        #    },
+          {"ch":'0',
+           "position":[100, 90],
+           "size":(40, 40),
+           },
+          {"ch":'1',
+           "position":[100, 50],
+           "size":(40, 100),
+           },
+          {"ch":'2',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'3',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'4',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
           {"ch":'5',
            "position":[100, 50],
            "size":(57, 100),
            },
-        #   {"ch":'6',
-        #    "position":[100, 50],
-        #    "size":(57, 100),
-        #    },
-        #   {"ch":'7',
-        #    "position":[100, 50],
-        #    "size":(57, 100),
-        #    },
-        #   {"ch":'8',
-        #    "position":[100, 50],
-        #    "size":(57, 100),
-        #    },
-        #   {"ch":'9',
-        #    "position":[100, 50],
-        #    "size":(57, 100),
-        #    },
+          {"ch":'6',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'7',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'8',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
+          {"ch":'9',
+           "position":[100, 50],
+           "size":(57, 100),
+           },
         ]
 
 chars = [ 
+        #  {"ch":'B',
+        #    "position":(260, 50),
+        #    "size":(110, 90),
+        #    },
+        #  {"ch":'N',
+        #    "position":(265, 35),
+        #    "size":(100, 100),
+        #    },
         #  {"ch":'T',
         #    "position":(252, 30),
         #    "size":(120, 110),
@@ -110,13 +115,17 @@ chars = [
         #    "position":(265, 40),
         #    "size":(100, 100),
         #    },
+        #  {"ch":'HE',
+        #    "position":(265, 40),
+        #    "size":(100, 100),
+        #    },
         #  {"ch":'SIN',
-        #    "position":(265, 55),
-        #    "size":(100, 80),
+        #    "position":(260, 55),
+        #    "size":(110, 80),
         #    },
         #  {"ch":'SAD',
-        #    "position":(265, 55),
-        #    "size":(100, 80),
+        #    "position":(260, 55),
+        #    "size":(110, 80),
         #    },
         #  {"ch":'TA',
         #    "position":(270, 30),
@@ -130,66 +139,78 @@ chars = [
         #    "position":(280, 40),
         #    "size":(70, 100),
         #    },
-         {"ch":'M',
-           "position":(280, 40),
-           "size":(70, 100),
-           },
-        #  'HE', 'L',  'V', 'Y'
+        #  {"ch":'Y',
+        #    "position":(270, 40),
+        #    "size":(90, 100),
+        #    },
+        #  {"ch":'L',
+        #    "position":(275, 35),
+        #    "size":(70, 110),
+        #    },
          ]
 
-# Make a random choice from the list
-random_1 = random.choice(numbers)
-random_2 = random.choice(numbers)
 
-random_char = random.choice(chars)
+for i in range(1):
+    # Open the background image
+    background = Image.open("templates/savari2.png")
 
-random_3 = random.choice(numbers)
-random_4 = random.choice(numbers)
-random_5 = random.choice(numbers)
-random_6 = random.choice(numbers)
+    # Make a random choice from the list
+    random_1 = random.choice(numbers)
+    random_2 = random.choice(numbers)
 
-mini_random_1 = random.choice(mini_numbers)
-while mini_random_1["ch"]=='0':
+    random_char = random.choice(chars)
+
+    random_3 = random.choice(numbers)
+    random_4 = random.choice(numbers)
+    random_5 = random.choice(numbers)
+    random_6 = random.choice(numbers)
+
     mini_random_1 = random.choice(mini_numbers)
-mini_random_2 = random.choice(mini_numbers)
+    while mini_random_1["ch"]=='0':
+        mini_random_1 = random.choice(mini_numbers)
+    mini_random_2 = random.choice(mini_numbers)
 
 
 
-overlaych = remove_background(Image.open(f"./chars/{random_char['ch']}.png"))
+    # if random_char['ch']=='HE':
+    #     overlaych = remove_background_HE(Image.open(f"./chars/{random_char['ch']}.png"))
+    #     # overlaych = remove_background(Image.open(f"./chars/{random_char['ch']}.png"))
+    # else:        
+    overlaych = remove_background(Image.open(f"./chars/{random_char['ch']}.png"))
 
-overlay1 = remove_background(Image.open(f"./chars/{random_1['ch']}.png"))
-overlay1 = overlay1.resize(random_1['size']) 
-background.paste(overlay1, [100, random_1['position'][1]])
+    overlay1 = remove_background(Image.open(f"./chars/{random_1['ch']}.png"))
+    overlay1 = overlay1.resize(random_1['size']) 
+    background.paste(overlay1, [100, random_1['position'][1]])
 
-overlay2 = remove_background(Image.open(f"./chars/{random_2['ch']}.png"))
-overlay2 = overlay2.resize(random_2['size']) 
-background.paste(overlay2, [180, random_2['position'][1]])
-
-
-position = random_char['position']  # Specify the coordinates (x, y)
-overlaych = overlaych.resize(random_char['size']) 
-background.paste(overlaych, position)
-
-overlay3 = remove_background(Image.open(f"./chars/{random_3['ch']}.png"))
-overlay3 = overlay3.resize(random_3['size']) 
-background.paste(overlay3, [390, random_3['position'][1]])
-
-overlay4 = remove_background(Image.open(f"./chars/{random_4['ch']}.png"))
-overlay4 = overlay4.resize(random_4['size']) 
-background.paste(overlay4, [470, random_4['position'][1]])
-
-overlay5 = remove_background(Image.open(f"./chars/{random_5['ch']}.png"))
-overlay5 = overlay5.resize(random_5['size']) 
-background.paste(overlay5, [550, random_5['position'][1]])
-
-overlay6 = remove_background(Image.open(f"./chars/{mini_random_1['ch']}.png"))
-overlay6 = overlay6.resize(mini_random_1['size']) 
-background.paste(overlay6, [655, mini_random_1['position'][1]])
-
-overlay7 = remove_background(Image.open(f"./chars/{mini_random_2['ch']}.png"))
-overlay7 = overlay7.resize(mini_random_2['size']) 
-background.paste(overlay7, [720, mini_random_2['position'][1]])
+    overlay2 = remove_background(Image.open(f"./chars/{random_2['ch']}.png"))
+    overlay2 = overlay2.resize(random_2['size']) 
+    background.paste(overlay2, [180, random_2['position'][1]])
 
 
-# Save the final image with the overlay
-background.save("output_image.png")
+    position = random_char['position']  # Specify the coordinates (x, y)
+    overlaych = overlaych.resize(random_char['size']) 
+    background.paste(overlaych, position)
+
+    overlay3 = remove_background(Image.open(f"./chars/{random_3['ch']}.png"))
+    overlay3 = overlay3.resize(random_3['size']) 
+    background.paste(overlay3, [390, random_3['position'][1]])
+
+    overlay4 = remove_background(Image.open(f"./chars/{random_4['ch']}.png"))
+    overlay4 = overlay4.resize(random_4['size']) 
+    background.paste(overlay4, [470, random_4['position'][1]])
+
+    overlay5 = remove_background(Image.open(f"./chars/{random_5['ch']}.png"))
+    overlay5 = overlay5.resize(random_5['size']) 
+    background.paste(overlay5, [550, random_5['position'][1]])
+
+    overlay6 = remove_background(Image.open(f"./chars/{mini_random_1['ch']}.png"))
+    overlay6 = overlay6.resize(mini_random_1['size']) 
+    background.paste(overlay6, [655, mini_random_1['position'][1]])
+
+    overlay7 = remove_background(Image.open(f"./chars/{mini_random_2['ch']}.png"))
+    overlay7 = overlay7.resize(mini_random_2['size']) 
+    background.paste(overlay7, [720, mini_random_2['position'][1]])
+
+
+    # Save the final image with the overlay
+    background.save(f"output/output_image_{i}.png")
--- remove_background.py
+++ remove_background.py
@@ -24,5 +24,45 @@ def remove_background(image):
     image.putdata(new_data)
 
     # Save the image with transparent background
-    image.save("4.png")
+    image.save("EIN_T.png")
     return image
+
+# def remove_background2():
+#     import cv2
+#     import numpy as np
+
+#     # Load the image
+#     image = cv2.imread(f"./chars/EIN.png")
+
+#     # Convert the image to grayscale
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#     # Apply thresholding to separate the object from the background
+#     _, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
+#     # Invert the binary image
+#     binary = cv2.bitwise_not(binary)
+
+#     # Apply bitwise AND operation to extract the object
+#     result = cv2.bitwise_and(image, image, mask=binary)
+
+#     # Save the result
+#     cv2.imwrite(f"./chars/EIN_T.png", result)
+#     image = cv2.imread(f"./chars/EIN_T.png")
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     cv2.imwrite(f"./chars/EIN_T.png", gray)
+#     image = Image.open(f"./chars/EIN_T.png")
+#     image = image.convert("RGBA")
+#     data = image.getdata()
+#     threshold = 50
+#     new_data = []
+#     for item in data:
+#         if item[0] > threshold and item[1] > threshold and item[2] > threshold:
+#             new_data.append((0, 0, 0, 255))
+#         else:
+#             new_data.append((255, 255, 255, 255))
+#     # Update the image data
+#     image.putdata(new_data)
+#     # Save the image with transparent background
+#     image.save(f"./chars/EIN_T.png")
+# remove_background2()
